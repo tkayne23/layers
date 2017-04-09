fe121c91119cc44d921c213e3c0d5d885c7f30d0
Sam Richard <sam.richard@gmail.com>
2017-04-09 10:55:40 -0600

    layout more handler

--- cloud/services/etl/etl.handler.js
+++ cloud/services/etl/etl.handler.js
@@ -1,6 +1,48 @@
+const AWS = require('AWS');
+const AquistionManager = require('./aquistion-manager');
+
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+const storage = {
+  upload: function(stream, filename) {
+    let s3 = new AWS.S3();
+    let params = {Bucket: 'layers-documents', Key: filename, Body: stream};
+    
+    s3.upload(params, function(err, data) {
+      console.log(err, data);
+    });
+  } 
+};
+
+module.exports.aquire = (event, context, callback) => {
+  const manager = new AquistionManager(storage, null);
+  const facilities = event.body.facilities;
+
+  manager.scrape(facilities, (err) => {
+    let response;
+    if (err) {
+      response = {
+        statusCode: 415,
+        body: JSON.stringify({
+          message: 'Shit broke, yo!',
+          input: event,
+        })
+      };
+    } else {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Successfully imported DSUs from ${facilities.length} documents`,
+          input: event,
+        })
+      };
+    }
+
+    callback(null, response);
+  });
+};
+
+module.exports.process = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -14,3 +56,18 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+// module.exports.turk = (event, context, callback) => {
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: 'Go Serverless v1.0! Your function executed successfully!',
+//       input: event,
+//     }),
+//   };
+
+//   callback(null, response);
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+// };
